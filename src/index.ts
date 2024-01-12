61f64569430cecac693b077bf6a27ca9512607f0
uwap <me@uwap.name>
2024-01-12 19:56:56 +0000

    Add build tools

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export function loop() {
+    console.log("Hello world!");
+}
---
+++ upload.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const fs = require('fs');
+var https = require('https');
+
+var data = {
+    branch: process.env.SCREEPS_API_BRANCH,         
+    modules: {
+        main: fs.readFileSync('./dist/screeps.js', 'utf-8').toString(),
+    }
+};
+
+var req = https.request({
+    hostname: 'screeps.com',
+    port: 443,
+    path: '/api/user/code',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        'X-Token': process.env.SCREEPS_API_TOKEN
+    }
+});
+
+req.write(JSON.stringify(data));
+req.end();
