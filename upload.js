10cd701e11c85045aabf5f2a5a3dbab39dc6493b
uwap <me@uwap.name>
2024-01-12 21:15:27 +0000

    upload.js: Error Handling

--- upload.js
+++ upload.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const fs = require('fs');
 var https = require('https');
+const { exit } = require('process');
 
 var data = {
     branch: process.env.SCREEPS_API_BRANCH,         
@@ -18,6 +19,23 @@ var req = https.request({
         'Content-Type': 'application/json; charset=utf-8',
         'X-Token': process.env.SCREEPS_API_TOKEN
     }
+}, (res) => {
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+        const response = JSON.parse(chunk);
+        if (response?.error != null) {
+            console.error(`ERROR: ${response.error}`);
+            exit(1);
+        } else if (response?.ok != null) {
+            console.log("Uploaded sucessfully");
+        } else {
+            console.log(chunk);
+            exit(1);
+        }
+    });
+    req.on('error', function(e) {
+        console.log('problem with request: ' + e.message);
+    });
 });
 
 req.write(JSON.stringify(data));
