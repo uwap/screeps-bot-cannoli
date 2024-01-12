483418092380ac0e58997245113d87d3dd95c090
uwap <me@uwap.name>
2024-01-12 21:21:01 +0000

    push script: run build automatically

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { registerProcess, runProcesses } from "./processes";
 
 registerProcess(() => {
-    console.log("Hello World!");
+    for (const s in Game.spawns) {
+        const spawn = Game.spawns[s];
+        spawn.spawnCreep([MOVE, WORK, CARRY], 'worker');
+    }
 })
 
 export function loop() {
